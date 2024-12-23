6ff5341bbe7f74e1d5cfdcf121403722392f86d4
Othmane Bouzidi <othmane.bouzidi@storebrand.no>
2024-12-24 00:32:46 +0100

    update

--- init.js
+++ init.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     handleClientLoad();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dateFrom = document.getElementById("date-from");
+    const dateTo = document.getElementById("date-to");
+    const today = new Date().toISOString().split("T")[0];
+
+    // Configura la fecha mínima para ambos campos
+    dateFrom.setAttribute("min", today);
+    dateTo.setAttribute("min", today);
+
+    // Ajusta la fecha mínima del campo "Dato til" en función de "Dato fra"
+    dateFrom.addEventListener("change", function () {
+        dateTo.setAttribute("min", dateFrom.value);
+    });
+});
